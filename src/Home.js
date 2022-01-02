dd4ac7ad2319f5841b00c50aa75a2eb0d7052d76
Ayatoullah Ramzy <40855411+ayatoullah@users.noreply.github.com>
2022-01-02 21:21:28 +0200

    refactor - trasfer add book to home

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,30 @@
 import React from "react";
 import Shelf from "./Shelf";
 
-const Home = ({currentlyReading , read, wantToRead, updateShelf}) => {
+const Home = ({ currentlyReading, read, wantToRead, ...props }) => {
   return (
-    <div className="list-books-content">
-      <div>
-        <Shelf books={currentlyReading} shelfName="currentlyReading" updateShelf = {updateShelf}/>
-        <Shelf books={read} shelfName="read" updateShelf = {updateShelf}/>
-        <Shelf books={wantToRead} shelfName="wantToRead" updateShelf = {updateShelf}/>
+    <>
+      <div className="list-books-content">
+        <div>
+          <Shelf
+            books={currentlyReading}
+            shelfName="currentlyReading"
+            updateShelf={props.updateShelf}
+          />
+          <Shelf books={read} shelfName="read" updateShelf={props.updateShelf} />
+          <Shelf
+            books={wantToRead}
+            shelfName="wantToRead"
+            updateShelf={props.updateShelf}
+          />
+        </div>
       </div>
-    </div>
+      <div className="open-search">
+        <button onClick={() => props.showSearchPage()}>
+          Add a book
+        </button>
+      </div>
+    </>
   );
 };
 
